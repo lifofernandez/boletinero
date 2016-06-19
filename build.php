1d47a2cef4dffbc4b970360eb2f74ffc592ffd0b
lifo fernandez <lifofernandez@gmail.com>
2016-06-19 02:49:06 +0200

    t

--- build.php
+++ build.php
@@ -5,7 +5,6 @@
 //======================================================================
 
 
-
 //-----------------------------------------------------
 // Importar librerias
 //-----------------------------------------------------
@@ -14,12 +13,12 @@
 // composer require twig/extensions
 require_once 'vendor/autoload.php';
 
+
 /*
 * Esto va si se isntala de a mano
 * require_once '../vendor/Twig-1.24.0/lib/Twig/Autoloader.php';
 * Twig_Autoloader::register();
 */
-
 
 
 
@@ -31,15 +30,15 @@ $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader);
 //$twig->addExtension(new Twig_Extensions_Extension_Intl());
 
-// Custom Filter 'slug'
-$filter = new Twig_SimpleFilter('slug', function ($string) {
-	// $string = transliterator_transliterate("Any-Latin; NFD; [:Nonspacing Mark:]
-	// Remove; NFC; [:Punctuation:] Remove; Lower();", $string);
-	// $string = preg_replace('/[-\s]+/', '-', $string);
-	// $string = trim($string, '-');
-	// return $string;
-});
-$twig->addFilter($filter);
+// // Custom Filter 'slug'
+// $filter = new Twig_SimpleFilter('slug', function ($string) {
+// 	// $string = transliterator_transliterate("Any-Latin; NFD; [:Nonspacing Mark:]
+// 	// Remove; NFC; [:Punctuation:] Remove; Lower();", $string);
+// 	// $string = preg_replace('/[-\s]+/', '-', $string);
+// 	// $string = trim($string, '-');
+// 	// return $string;
+// });
+// $twig->addFilter($filter);
 
 // Templates
 $index = $twig->loadTemplate('boletin.html.twig');
@@ -49,14 +48,11 @@ $index = $twig->loadTemplate('boletin.html.twig');
 //-----------------------------------------------------
 // Cargar Feeds .json
 //-----------------------------------------------------
-$curret_iyu = 'boletin_2016-06';
 
-$contenidosJson = file_get_contents("contenidos/".$curret_iyu.".json");
+$current = 'boletin_2016-06';
 
-
+$contenidosJson = file_get_contents("contenidos/".$current.".json");
 $contenidos = json_decode($contenidosJson,true); // 'true' devuelve  array
-
-
 
 
 
@@ -66,14 +62,8 @@ $contenidos = json_decode($contenidosJson,true); // 'true' devuelve  array
 
 $output = $index->render($contenidos);
 
-
-
-$file = $curret_iyu.'.html';
-
-// Write the contents back to the file
+$file = $current.'.html';
 file_put_contents($file, $output);
-
-
 
 
 
